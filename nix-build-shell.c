d1a7616ea17e6fcb760a828a12fe6117278a91db
ge86mak <ge86mak@mytum.de>
2022-07-23 06:22:33 -0700

    test 2 in progress

--- nix-build-shell.c
+++ nix-build-shell.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+#include <sched.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -58,15 +60,43 @@ int main(int argc, const char **argv)
     parse_env_vars_file(env_vars_fp, shell_path, match_pattern);
     fclose(env_vars_fp);
 
-    // execute the command
+    // usernamespace setting
+    int userns_ret = unshare(CLONE_NEWUSER);
+    if (userns_ret != 0)
+        perror("failure in usernamespace unshare");
+    // write to /proc/self/uid_map and /proc/self/gid_map
+    FILE *uid_map_fp = fopen("/proc/self/uid_map", "r+");
+    if (uid_map_fp == NULL)
+    {
+        perror("uid_map file open failure");
+        exit(1);
+    }
+    FILE *gid_map_fp = fopen("/proc/self/gid_map", "r+");
+    if (gid_map_fp == NULL)
+    {
+        perror("gid_map file open failure");
+        exit(1);
+    }
+    // fputs("1000 0 4294967295", uid_map_fp);
+    // fputs("100 0 4294967295", gid_map_fp);
+    fclose(uid_map_fp);
+    fclose(gid_map_fp);
+
+    // execute the basic command
     char *exec_argv[3 + argc];
     exec_argv[0] = shell_path;
     exec_argv[1] = "-c";
-    exec_argv[2] = "source /tmp/nix-build-hello.drv-0/env-vars; exec \"$@\"";
+    exec_argv[2] = "source /tmp/nix-build-hello.drv-0/env-vars; exec \"$@\""; // /build/env-vars after successful setup
     exec_argv[3] = "--";
     for (int i = 0; i < argc - 2; i++)
         exec_argv[4 + i] = argv[2 + i];
-    exec_argv[2+argc] = (char*)0;
-    // char* exec_argv[] = {"/nix/store/a4yw1svqqk4d8lhwinn9xp847zz9gfma-bash-4.4-p23/bin/bash", "-c", "source /tmp/nix-build-hello.drv-0/env-vars; exec \"$@\"", "--", "bash", "-c", "echo $HOME", (char*)0};
+    exec_argv[2 + argc] = (char *)0;
+    
+    // debug
+    // char* exec_argv[] = {"/nix/store/a4yw1svqqk4d8lhwinn9xp847zz9gfma-bash-4.4-p23/bin/bash", "-c", "source /tmp/nix-build-hello.drv-0/env-vars; exec \"$@\"", "--", "cat", "/proc/$$/uid_map", (char*)0};
+    // for (int i = 0; i < argc + 2; i++)
+    //     printf("%s\n",exec_argv[i]);
+    // fflush( stdout );
+
     execv(shell_path, exec_argv);
 }
